0d56432431a14f8e2e34214464a16db0accee7a3
opportunato <ryu.gordeyev@gmail.com>
2015-12-28 13:49:21 +0200

    Added input and rotation

--- flappy-bird-reborn/src/js/prefabs/bird.js
+++ flappy-bird-reborn/src/js/prefabs/bird.js
@@ -18,10 +18,16 @@
   Bird.prototype = Object.create(Phaser.Sprite.prototype);  
   Bird.prototype.constructor = Bird;
 
+  Bird.prototype.flap = function() { 
+     this.body.velocity.y = -400;
+
+     this.game.add.tween(this).to({angle: -40}, 100).start();
+  };
+
   Bird.prototype.update = function() {
-
-    // write your prefab's specific update code here
-
+    if (this.angle < 90) {
+      this.angle += 2.5;
+    }
   };
 
   window['flappy-bird-reborn'] = window['flappy-bird-reborn'] || {};
